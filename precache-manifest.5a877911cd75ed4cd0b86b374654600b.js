292afef8f00168d5555b7a9452b0923ab049e55d
Katgordon <kat.gordon2@gmail.com>
2019-04-30 21:50:18 -0500

    Updates

---
+++ precache-manifest.5a877911cd75ed4cd0b86b374654600b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc1cd4c6efdc80abfab6307aa243d39b",
+    "url": "/Clickygame/index.html"
+  },
+  {
+    "revision": "1a2ed8cb8c912eba3d4a",
+    "url": "/Clickygame/static/css/main.27755312.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/Clickygame/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "1a2ed8cb8c912eba3d4a",
+    "url": "/Clickygame/static/js/main.fa62c307.chunk.js"
+  },
+  {
+    "revision": "1cff9d9d67f6463adea2",
+    "url": "/Clickygame/static/js/runtime~main.848eaae1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Clickygame/static/media/logo.5d5d9eef.svg"
+  }
+]);
